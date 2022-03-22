62d22b893cd3d2cf5c550033a9b01179deafd448
Eleni Partakki <47921764+epartakki@users.noreply.github.com>
2022-03-21 23:48:27 -0400

    Update user_controller.rb

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
   skip_before_action :authorized, only: [:new, :create]
 
-  # sign-up page controller
+  # sign-up
   def new
     @user = User.new
   end
 
-  # processing the sign-up request
+  # processing the request
   def create
     @user = User.create(params.require(:user).permit(:first_name, :last_name, :username,
    :password, :email))
