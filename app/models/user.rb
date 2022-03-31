0630659aef66e942f1f65af7a530e3f867e5fe95
sunniraleigh <sraleigh@smith.edu>
2022-03-31 13:47:21 -0400

    update user validations, add user migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
     has_secure_password
 
     validates_presence_of   :identifier,
-                            :firstName,
-                            :lastName,
+                            :first_name,
+                            :last_name,
                             :email,
-                            :phoneNumber,
-                            :milesRidden,   #depends on rides
-                            :timesRidden    #depends on rides
+                            :phone_number,
+                            :miles_ridden,   #depends on rides
+                            :ride_count    #depends on rides
     validates_uniqueness_of :identifier
 
     belongs_to :rides, class_name: :Ride, foreign_key: :user_id
--- db/migrate/20220330211042_create_users.rb
+++ db/migrate/20220330211042_create_users.rb
@@ -1,8 +1,15 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
+        t.integer   :identifier
+        t.string    :first_name
+        t.string    :last_name
+        t.string    :email
+        t.string    :phone_number
+        t.integer   :miles_ridden
+        t.integer   :ride_count
 
-      t.timestamps
+        t.timestamps
     end
   end
 end
