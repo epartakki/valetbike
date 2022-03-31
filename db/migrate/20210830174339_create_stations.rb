4118b043570a586ead79fb76cb20172a763e4796
sunniraleigh <sraleigh@smith.edu>
2022-03-31 13:43:53 -0400

    add station migrations

--- db/migrate/20210830174339_create_stations.rb
+++ db/migrate/20210830174339_create_stations.rb
@@ -2,8 +2,15 @@ class CreateStations < ActiveRecord::Migration[6.1]
   def change
     create_table :stations do |t|
       t.integer :identifier
-      t.string :name
-      t.string :address
+      t.string  :name
+      t.string  :address_line1
+      t.string  :address_city
+      t.string  :address_state
+      t.string  :address_country
+      t.integer :total_slots
+      t.integer :available_slots
+      t.integer :lng
+      t.integer :lat
 
       t.timestamps
     end
