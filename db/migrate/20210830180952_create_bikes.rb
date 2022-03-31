a21e1d0381dc101d7bb80415a795d15cd9a3fd67
sunniraleigh <sraleigh@smith.edu>
2022-03-31 13:49:54 -0400

    add bike migrations

--- db/migrate/20210830180952_create_bikes.rb
+++ db/migrate/20210830180952_create_bikes.rb
@@ -2,6 +2,9 @@ class CreateBikes < ActiveRecord::Migration[6.1]
   def change
     create_table :bikes do |t|
       t.integer :identifier
+      t.string  :bike_model
+      t.integer :charge
+      t.string  :condition
       t.integer :current_station_id
 
       t.timestamps
