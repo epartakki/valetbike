d3d0500fa5231875001fab7ee4985e58e1b3e49e
sunniraleigh <sraleigh@smith.edu>
2022-03-31 13:36:03 -0400

    add ride migrations

--- db/migrate/20220328200931_create_rides.rb
+++ db/migrate/20220328200931_create_rides.rb
@@ -1,8 +1,18 @@
 class CreateRides < ActiveRecord::Migration[6.1]
-  def change
-    create_table :rides do |t|
+  
+    def change
+        create_table :rides do |t|
+            t.integer   :identifier
+            t.datetime  :start_time
+            t.datetime  :end_time
+            t.time      :duration
+            t.integer   :user_id
+            t.integer   :bike_id
+            t.integer   :start_station_id
+            t.integer   :end_station_id
 
-      t.timestamps
+            t.timestamps
+        end
     end
-  end
+    
 end
