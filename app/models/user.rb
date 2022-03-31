cb3ed23fb37dadb567b154925658d8d75a77cf99
sunniraleigh <sraleigh@smith.edu>
2022-03-30 20:19:27 -0400

    add user cols, change bike tabs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    validates_presence_of   :identifier,
+                            :name,
+                            :email,
+                            :phoneNumber,
+                            :milesRidden,
+                            :timesRidden
+    validates_uniqueness_of :identifier
 end
