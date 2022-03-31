d5a6c572eaa043936d1c0c4aedd414ac4da11524
sunniraleigh <sraleigh@smith.edu>
2022-03-31 11:14:23 -0400

    add station asociations

--- app/models/station.rb
+++ app/models/station.rb
@@ -5,5 +5,6 @@ class Station < ApplicationRecord
     validates_uniqueness_of :identifier
   
   has_many :docked_bikes, class_name: :Bike, foreign_key: :current_station_id
-  
+  belongs_to :start_ride, class_name: :Ride, foreign_key: :start_ride_id
+  belongs_to :end_ride, class_name: :Ride, foreign_key: :end_ride_id
 end
