d538c40faf9d736a8a062cf21169244699509c21
sunniraleigh <sraleigh@smith.edu>
2022-04-04 11:23:57 -0400

    update bike model + migration

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,8 +1,6 @@
 class Bike < ApplicationRecord
-    validates_presence_of   :identifier,
-                            :bike_model,
-                            :charge,
-                            :condition
+    validates_presence_of   :identifier
+
     validates_uniqueness_of :identifier
 
     belongs_to :current_station, class_name: :Station, foreign_key: :current_station_id, optional: true
--- db/migrate/20210830180952_create_bikes.rb
+++ db/migrate/20210830180952_create_bikes.rb
@@ -1,9 +1,6 @@
 class CreateBikes < ActiveRecord::Migration[6.1]
   def change
     create_table :bikes do |t|
-      t.string  :bike_model
-      t.integer :charge
-      t.string  :condition
       t.integer :current_station_id
 
       t.timestamps
