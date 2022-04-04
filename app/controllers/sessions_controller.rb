174d370351c7ed682bbe7907d746203591d18b49
epartakki <epartakki@smith.edu>
2022-04-04 19:31:03 -0400

    fixing some errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,106 +1,29 @@
 class SessionsController < ApplicationController
 
+  skip_before_action :authorized, only: [:new, :create, :welcome]
 
-    skip_before_action :authorized, only: [:new, :create, :welcome, :about, :payment, :how_it_works, :FAQ]
-  
-  
-    def new
-    end
-  
-    # Check user's input of username and password
-    def create
-      @user = User.find_by(username: params[:username])
-      if @user && @user.authenticate(params[:password])
-  
-          session[:user_id] = @user.id
-  
-          redirect_to '/authorized'
-      else
-          flash[:error] = "Incorrect username or password" # Send error when redirected
-          redirect_to '/login'
-      end
-  
-    end
-  
-    def login
-    end
-  
-    def welcome
-    end
-  
-    def about
-    end
-  
-    # This is for map page
-    def page_requires_login
-      @stations = Station.all.order(identifier: :asc)
-      @bikes = Bike.all.order(identifier: :asc)
-    end
-  
-    def payment
-    end
-  
-    # User can type in the bike code to rent the bike
-    # This page is aware of what station is specified
-    def checkout
-      # Check if the query of station_identifier exists in url
-      if params[:station_identifier]
-        session[:station_identifier] = params[:station_identifier].to_i
-      end
-      @station = Station.find_by_identifier(session[:station_identifier])
-    end
-  
-    # Process check-out request
-    def check
-      # if user has a bike checked out already, they get redirected to check-in page
-      if current_user.current_bike_id
-        flash[:alart] = "You cannot check out more than one bike. Please return your bike to a station"
-        redirect_to '/checkin' and return
-      end
-      b = Bike.find_by_identifier(params[:bikeid])
-      # Check if the bike user is trying to check out exists at the station specified
-      if b && session[:station_identifier] == b.current_station_identifier
-        current_user.update_attribute(:current_bike_id, params[:bikeid])
-        b.update_attribute(:current_station_identifier, nil)
-        redirect_to '/ride'
-      else
-        # if the bike doesn't exist in the specified station, send error
-        flash[:error] = "Could not find the bike at this station"
-        redirect_to '/checkout'
-      end
-    end
-  
-    def ride
-      @bike = Bike.find_by_identifier(current_user.current_bike_id)
-      @stations = Station.all.order(identifier: :asc)
-      @bikes = Bike.all.order(identifier: :asc)
-    end
-  
-    def about
-    end
-  
-    # Process logout request
-    def logout
-      session[:user_id]= nil
-      redirect_to '/welcome'
-    end
-  
-    def how_it_works
-    end
-  
-    def FAQ
-    end
-  
-    def checkin
-      @stations = Station.all.order(identifier: :asc)
-    end
-  
-    #Processes bike check in
-    def process_checkin
-      @bike = Bike.find_by_identifier(current_user.current_bike_id)
-      @bike.update_attribute(:current_station_identifier, params[:station_identifier])
-      current_user.update_attribute(:current_bike_id, nil)
-    end
-  
+  def new
   end
-  
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+
+
+        session[:user_id] = @user.id
+
+        redirect_to '/welcome'
+    else
+        redirect_to '/login'
+    end
+
+  end
+
+  def login
+  end
+
+  def welcome
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,15 @@ Rails.application.routes.draw do
   
   resources :users, only: [:new, :create, :checkout]
 
-  get 'login', to: 'stations#new'
-  post 'login', to: 'stations#create'
-  get 'welcome', to: 'stations#welcome'
-  get 'about', to: 'stations#about'
-  get 'how_it_works', to: 'stations#how_it_works'
-  get 'FAQ', to: 'stations#FAQ'
-  get 'authorized', to: 'stations#page_requires_login'
-  get 'payment', to: 'stations#payment'
-  get 'checkout', to: 'stations#checkout'
-  post 'checkout', to: 'stations#check'
-  post 'logout', to: 'stations#logout'
-  get 'ride', to: 'stations#ride'
-  get 'checkin', to: 'stations#checkin'
-  post 'checkin', to: 'stations#process_checkin'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  get 'about', to: 'sessions#about'
+  get 'FAQ', to: 'sessions#FAQ'
+  get 'authorized', to: 'sessions#page_requires_login'
+  get 'payment', to: 'sessions#payment'
+  post 'logout', to: 'sessions#logout'
 
   resources :places
-  root to: "stations#index"
+  root to: "sessions#welcome"
 end
