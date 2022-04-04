6b39e46bcb76f3371739b130a433a2cd6d12fdbe
sunniraleigh <sraleigh@smith.edu>
2022-04-04 11:22:59 -0400

    update user model + migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,15 @@ class User < ApplicationRecord
     has_secure_password
 
     validates_presence_of   :identifier,
+                            :username,
+                            :password,
                             :first_name,
                             :last_name,
                             :email,
                             :phone_number,
                             :miles_ridden,   #depends on rides
                             :ride_count    #depends on rides
+    
     validates_uniqueness_of :identifier
 
     belongs_to :rides, class_name: :Ride, foreign_key: :user_id
