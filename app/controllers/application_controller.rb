35b39879959c19fd26b9e7ccc9734b87fa5597b5
sunniraleigh <sraleigh@smith.edu>
2022-04-05 14:47:27 -0400

    setup current user from session in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,27 @@
 class ApplicationController < ActionController::Base
-    
-    before_action :authorized
-    helper_method :current_user
-    helper_method :logged_in?
-  
+
+    private
+
+    # Find user with id stored in the session
     def current_user
-        User.find_by(id: session[:user_id])
+        @_current_user ||= session[:current_user_id] && # if there is not a current user, assign to the user of the current session
+            User.find_by(id: session[:current_user_id])
     end
+    
+    # before_action :authorized
+    # helper_method :current_user
+    # helper_method :logged_in?
   
-    def logged_in?
-        !current_user.nil?
-    end
+    # def current_user
+    #     User.find_by(id: session[:user_id])
+    # end
   
-    def authorized
-      redirect_to '/welcome' unless logged_in?
-    end
+    # def logged_in?
+    #     !current_user.nil?
+    # end
+  
+    # def authorized
+    #   redirect_to '/welcome' unless logged_in?
+    # end
 
 end
