5bb458d4662b7bcf89bc757fb70de74ba9ee9a9d
sunniraleigh <sraleigh@smith.edu>
2022-04-07 20:03:25 -0400

    simplify station model + migrations

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,11 +1,8 @@
 class Station < ApplicationRecord
     validates_presence_of   :identifier,
                             :name,
-                            :has_kiosk,
-                            :needs_maintenance,
-                            :total_slots,
-                            :available_slots, # changes
-                            :address
+                            :lat,
+                            :lng
     validates_uniqueness_of :identifier
   
     has_many :docked_bikes, class_name: :Bike, foreign_key: :current_station_id
--- db/migrate/20210830174339_create_stations.rb
+++ db/migrate/20210830174339_create_stations.rb
@@ -2,11 +2,8 @@ class CreateStations < ActiveRecord::Migration[6.1]
   def change
     create_table :stations do |t|
       t.string  :name
-      t.boolean :has_kiosk
-      t.boolean :needs_maintenance
-      t.integer :total_slots
-      t.integer :available_slots
-      t.integer :address
+      t.decimal :lat
+      t.decimal :lng
 
       t.timestamps
     end
