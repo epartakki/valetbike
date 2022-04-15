c0225c7ed2cbc32807d5105add8b3e7fa40f27fd
sunniraleigh <sraleigh@smith.edu>
2022-04-14 21:29:58 -0400

    adjust user models?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-    has_secure_password # using bcrypt gem
+    # has_secure_password # using bcrypt gem
 
-    validates_presence_of   :identifier,
-                            :username,
-                            :password
+    # validates_presence_of   :identifier,
+    #                         :username,
+    #                         :password
     
-    validates_uniqueness_of :identifier,
-                            :username
+    # validates_uniqueness_of :identifier,
+    #                         :username
 
     belongs_to :rides, class_name: :Ride, foreign_key: :user_id
 end
