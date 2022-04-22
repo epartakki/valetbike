ab91b3f99346c888f39474d45d60767dbe264fd1
epartakki <epartakki@smith.edu>
2022-04-21 20:06:32 -0400

    pending

--- db/schema.rb
+++ db/schema.rb
@@ -39,20 +39,20 @@ ActiveRecord::Schema.define(version: 2022_04_04_042316) do
   end
 
   create_table "stations", charset: "utf8mb4", collation: "utf8mb4_unicode_520_ci", force: :cascade do |t|
+    t.integer "identifier"
     t.string "name"
+    t.string "address"
     t.decimal "lat", precision: 10
     t.decimal "lng", precision: 10
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_unicode_520_ci", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    t.string "first_name"
-    t.string "last_name"
+    t.string "name"
     t.string "email"
-    t.string "phone_number"
-    t.integer "miles_ridden"
-    t.integer "ride_count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
