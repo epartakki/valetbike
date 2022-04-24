089bd42c6b060a5825132ad5f42184dfb8c2643c
amarrap <amarra@smith.edu>
2022-04-24 14:26:07 -0400

    comment out bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,14 @@ class User < ApplicationRecord
     # https://stackoverflow.com/questions/12746280/define-a-unique-primary-key-based-on-2-columns
     # has_secure_password # using bcrypt gem
 
-    validates_presence_of   #:identifier,
-                            :username,
-                            :password
+    # validates_presence_of   :identifier,
+    #                         :username,
+    #                         :password
     
     # validates_uniqueness_of :identifier
 
+    validates_presence_of   :username
+                            :password
 
-    
     has_many :rides, class_name: :Ride, foreign_key: :user_id
 end
