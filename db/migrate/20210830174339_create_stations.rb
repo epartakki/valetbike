9adb402d7abf4d2dbe6bcf755122f905505aca0a
sunniraleigh <sraleigh@smith.edu>
2022-04-25 14:46:56 -0400

    add extra station information to seed and migration

--- db/migrate/20210830174339_create_stations.rb
+++ db/migrate/20210830174339_create_stations.rb
@@ -4,6 +4,10 @@ class CreateStations < ActiveRecord::Migration[6.1]
       t.integer :identifier
       t.string  :name
       t.string  :address
+      t.boolean :has_kiosk
+      t.boolean :needs_maintenance
+      t.integer :total_slots
+      t.integer :available_slots
       t.decimal :lat, :precision => 16, :scale => 13
       t.decimal :lng, :precision => 16, :scale => 13
 
